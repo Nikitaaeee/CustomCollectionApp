610dfb7e03363ce2508da636c2b5df8dafda96e6
Nikita Kirshin <Nik.a.kirshin@gmail.com>
2022-11-16 22:12:13 +0300

    Deleted Unused Code

--- CustomCollectionApp/Source/ListModule/Provider/Network/ListNetworkManager.swift
+++ CustomCollectionApp/Source/ListModule/Provider/Network/ListNetworkManager.swift
@@ -25,20 +25,6 @@ struct ListNetworkManager {
     }
 }
 
-// MARK: - Private
-
-private extension ListNetworkManager {
-    func handleNetworkResponse(_ response: HTTPURLResponse) -> Result<String, NetworkResponse> {
-        switch response.statusCode {
-        case 200...299: return .success(NetworkResponse.ok.rawValue)
-        case 400...499: return .failure(NetworkResponse.authenticationError)
-        case 500...599: return .failure(NetworkResponse.badRequest)
-        case 600: return .failure(NetworkResponse.outdated)
-        default: return .failure(NetworkResponse.failed)
-        }
-    }
-}
-
     // MARK: - Constants
 
 private enum Constants {
